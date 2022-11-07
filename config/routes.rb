2b9bf8684f382a3081d549e5e38aedfe25e813c7
Dillon <dpapenfu@chicagobooth.edu>
2022-11-07 16:21:35 +0000

    users index mostly works

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
-
+ get("/users", {:controller=> "users" , :action=> "index"})
+ get("/users/path_id", {:controller=> "users" , :action=> "index"})
+#get("/photos", {:controller: photos ; :action})
 end
