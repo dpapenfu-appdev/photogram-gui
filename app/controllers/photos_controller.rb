14748ae7d722957b4c591965a436e9a8d41c6eb8
Dillon <dpapenfu@chicagobooth.edu>
2022-11-09 17:23:01 +0000

    Read stuff is done. Just C, U, and D left

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,4 +5,11 @@ class PhotosController < ApplicationController
       @list_of_photos = matching_photos.order({ :created_at=> :desc})
       render ({:template => "photo_templates/index.html.erb"})
     end
+
+    def show
+        url_id = params.fetch("path_id")
+        matching_photos = Photo.where({:id=>url_id})
+        @the_photo = matching_photos.first
+        render ({:template=> "photo_templates/show.html.erb"})
+    end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
  get("/users", {:controller=> "users" , :action=> "index"})
  get("/users/:path_id", {:controller=> "users" , :action=> "show"})
-get("/photos", {:controller=> "photos" , :action=> "index"})
+ get("/photos", {:controller=> "photos" , :action=> "index"})
+ get("/photos/:path_id", {:controller=> "photos", :action=> "show"})
 end
